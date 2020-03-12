80e884eb430b89324c4de1c6a76877918b68cc60
lewisclark4 <lewis-clark@hotmail.co.uk>
2020-03-12 18:32:40 +0000

    add script.js file, added google maps api, and loaded map to page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: {
+            lat: 53.483959,
+            lng: -2.244644
+        }
+    });
+}
